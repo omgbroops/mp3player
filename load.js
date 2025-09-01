c02a092d85881c5327dd7231024ea6955dd1943d
omgbroops <omgofficial2021@gmail.com>
2025-09-01 15:07:12 -0500

    Update load.js

--- load.js
+++ load.js
@@ -1,3 +1,6 @@
-const ver = "v2.5r";
+let ver = "v2.6r";
+try {
+  ver = version;
+} catch (e) { }
 
-fetch('https://cdn.jsdelivr.net/gh/omgbroops/mp3player@' + ver + '/index.html').then(response => response.text()).then(html => { const container = document.querySelector('body'); container.innerHTML = '<!DOCTYPE html>' + '<html>' + '<head>' + '<title>New Tab</title>' + '</head>' + '<body>' + '<script>loadSessionDB();</script>' + html + '</body>' + '</html>'; container.querySelectorAll('script').forEach(oldScript => { const newScript = document.createElement('script'); if (oldScript.src) { newScript.src = oldScript.src; newScript.async = false; } else { newScript.textContent = oldScript.textContent; } oldScript.parentNode.replaceChild(newScript, oldScript); }); });
+fetch('https://cdn.jsdelivr.net/gh/omgbroops/mp3player@' + ver + '/index.html').then(response => response.text()).then(html => { const container = document.querySelector('body'); container.innerHTML = '<!DOCTYPE html>' + '<html>' + '<head>' + '<title>New Tab</title>' + '</head>' + '<body>' + html + '</body>' + '<script>loadSessionDB();</script></html>'; container.querySelectorAll('script').forEach(oldScript => { const newScript = document.createElement('script'); if (oldScript.src) { newScript.src = oldScript.src; newScript.async = false; } else { newScript.textContent = oldScript.textContent; } oldScript.parentNode.replaceChild(newScript, oldScript); }); });
